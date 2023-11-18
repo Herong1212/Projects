932d4b7dde8553053cf83d25eead205792500471
ghr <18463373955@163.com>
2023-11-18 16:43:35 +0800

    a

--- ch5-循环/hm_17_while...else之continue.py
+++ ch5-循环/hm_17_while...else之continue.py
@@ -3,7 +3,7 @@ while i <= 5:
     if i == 3:
         print('这遍说的不真诚')
         i += 1
-        break
+        continue
     print('媳妇儿. 我错了')
     i += 1
 else:
---
+++ ch5-循环/hm_18_for...else.py
@@ -0,0 +1,5 @@
+str1 = "itheima"
+for i in str1:
+    print(i)
+else:
+    print('循环正常结束之后执行的代码')
