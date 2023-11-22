66fa47cc99c3f90dbf29265b1eff3170a68bc866
ghr <18463373955@163.com>
2023-11-22 20:35:24 +0800

    0

---
+++ ch09-字典/hm_01_创建字典.py
@@ -0,0 +1,13 @@
+# {} 键值对  各个键值对用逗号隔开
+
+# 1.有数据的字典: name 的值 Tom,  age 的值 20,  gender 的值为 男
+dict1 = {"name": "Tom", "age": 20, "gender": "男"}
+print(dict1)
+print(type(dict1))
+
+# 2.创建空字典
+dict2 = {}
+print(type(dict2))
+
+dict3 = dict()
+print(type(dict3))
