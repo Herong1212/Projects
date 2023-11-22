f64831ef9b31acc4eed6796b68c6c43a12e4585a
ghr <18463373955@163.com>
2023-11-22 22:34:12 +0800

    11.22

---
+++ ch11-公共操作/hm_01_公共操作之运算符加号.py
@@ -0,0 +1,18 @@
+str1 = 'aaa'
+str2 = 'bbb'
+
+list1 = [1, 2]
+list2 = [10, 20]
+
+t1 = (1, 2)
+t2 = (10, 20)
+
+dict1 = {'name': 'Tom'}
+dict2 = {"age": 20}
+
+# + ：合并
+print(str1 + str2)
+print(list1 + list2)
+print(t1 + t2)
+
+# print(dict1 + dict2)    # 报错
---
+++ ch11-公共操作/hm_02_公共操作之运算符乘号.py
@@ -0,0 +1,13 @@
+str1 = 'a'
+list1 = ['hello']
+t1 = ('world',)
+
+# * : 复制
+print(str1 * 5)
+
+# 打印 10 个 -
+print('-' * 10)
+
+print(list1 * 5)
+
+print(t1 * 5)
